da2ecdef142e2cb19ece96ad7a7e7809798c8b75
Ahmed8008 <88592717+Ahmed8008@users.noreply.github.com>
2024-08-12 12:08:29 +0500

    Update testconnection.ts

--- pages/testconnection.ts
+++ pages/testconnection.ts
@@ -1,18 +1,1 @@
-import mysql from 'mysql2/promise';
 
-async function testConnection() {
-  try {
-    const connection = await mysql.createConnection({
-        host: 'fdb1030.awardspace.net',
-  user: '4516115_registrationdbb',
-  password: 'Ahmed12@',
-  database: '4516115_registrationdbb',
-      });
-    console.log('Database connection successful');
-    await connection.end();
-  } catch (error) {
-    console.error('Database connection error:', error);
-  }
-}
-
-testConnection();
